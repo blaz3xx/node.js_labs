87a374f696e3efca4031b3d59c98474f3f3b5f38
Orsted7true <orsted7true@gmail.com>
2026-02-25 20:20:53 +0200

    second commit

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const teamInfoData = {
 		role: 'Developer',
 		bio: 'Bogdan is a skilled developer with expertise in JavaScript and Node.js. He enjoys building scalable web applications and is passionate about open-source projects.',
 		skills: ['JavaScript', 'Node.js', 'Express', 'MongoDB'],
+		photo: "bohdan.jpg",
 		},
 	2: {
 		id: 2,
@@ -22,6 +23,7 @@ const teamInfoData = {
 		role: 'Designer',
 		bio: 'Andrii is a talented designer with a keen eye for aesthetics and user experience. He has a background in graphic design and specializes in creating visually appealing interfaces.',
 		skills: ['Graphic Design', 'UI/UX', 'Adobe Photoshop', 'Figma'],
+		photo: "andrii.jpg",
 	},
 	3: {
 		id: 3,
@@ -29,6 +31,7 @@ const teamInfoData = {
 		role: 'Project Manager',
 		bio: 'Angelina is an experienced project manager who excels at coordinating teams and ensuring projects are delivered on time. She has a strong background in agile methodologies and is skilled at communication and organization.',
 		skills: ['Project Management', 'Agile', 'Scrum', 'Communication'],
+		photo: "angelina.jpg",
 	},
 	4: {
 		id: 4,
@@ -36,18 +39,10 @@ const teamInfoData = {
 		role: 'QA Engineer',
 		bio: 'Artem is a meticulous QA engineer who is dedicated to ensuring the quality of software products. He has experience in both manual and automated testing and is proficient in various testing tools.',
 		skills: ['Manual Testing', 'Automated Testing', 'Selenium', 'JIRA'],
+		photo: "artem.jpg",
 	},
 };
 
-/* app.get('/student/:name', (req, res) => {
-	const studentInfo = teamInfoData[req.params.id];
-	
-	if (studentInfo) {
-		res.render('student', { student: studentInfo });
-	} else {
-		res.status(404).render('error', { message: 'Student not found' });
-	}
-}); */
 
 app.get('/student/:id', (req, res) => {
     
@@ -57,7 +52,7 @@ app.get('/student/:id', (req, res) => {
     // Шукаємо студента в масиві
     const foundStudent = teamInfoData[studentId];
 
-    // Перевірка на помилку (якщо ввели ID 999)
+    // Перевірка на помилку 
     if (!foundStudent) {
         // Можна відрендерити сторінку помилки або просто надіслати текст
         return res.status(404).send('<h1>Помилка 404: Такого студента у нас немає :(</h1><a href="/">На головну</a>');
